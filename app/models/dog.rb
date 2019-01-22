7bfa663edbb3d438c93e73051aa9c88351e78a31
Joey Cho <cjy12182000@naver.com>
2019-01-22 12:36:33 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,16 @@ class Dog
   
   attr_accessor :name, :breed, :age
   
+  @@all = 0;
+  
   def initialize(name, breed, age)
     @name = name;
     @breed = breed;
     @age = age;
+    
+  @@all+=1;
+  
   end
+  
   
 end
